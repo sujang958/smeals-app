cd5c3441d0af7ab5a274d8a3f995ca471fd2f4eb
unknown <enter7377@naver.com>
2022-05-16 22:14:55 +0900

    feat: add react navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createStackNavigator } from "@react-navigation/stack"
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import "react-native-gesture-handler"
+import HomeScreen from "./screens/homet"
+
+const RootStack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <RootStack.Screen name="Home" component={HomeScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/homet.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import { Text, View } from "react-native"
+
+const HomeScreen: FC = () => {
+  return (
+    <View>
+      <Text>hidf</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
