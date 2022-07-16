ba87b42cc851a12f6915c0adf005cf0e0c9dca62
sujang958 <enter7377@naver.com>
2022-07-16 20:16:07 +0900

    imprv: change function's region'

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions"
 import V1App from "./v1"
 
-export const v1 = functions.https.onRequest(V1App)
+export const v1 = functions
+  .region("asia-northeast3", "asia-northeast2")
+  .https.onRequest(V1App)
